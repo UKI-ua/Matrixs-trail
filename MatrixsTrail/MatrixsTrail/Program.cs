e5ccc21e614c8206b4d96963593d93769aaed620
Yurii Radchenko <uyrrar@gmail.com>
2021-03-07 18:36:42 +0200

    Added code

---
+++ MatrixsTrail/MatrixsTrail/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MatrixsTrail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfColumns;
+            int numberOfLines;
+
+            do
+            {
+                Console.Write("Enter number of lines: ");
+                numberOfLines = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter number of columns: ");
+                numberOfColumns = int.Parse(Console.ReadLine());
+
+                if (numberOfLines < 1 || numberOfColumns < 1)
+                    Console.WriteLine("Wrong input! Try again");
+            }
+            while (numberOfLines < 1 || numberOfColumns < 1);
+
+            try
+            {
+                var matrix = new Matrix(numberOfLines, numberOfColumns);
+
+                Console.WriteLine("Your matrix is:");
+                matrix.PrintOnConsole();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
